aba1869be23efd3e9501c0da16b3f08e70adc34b
mohitacharya <mohitacharya@bajratechnologies.com>
2022-04-05 22:36:39 +0545

    feat: added vailation on vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 class Vehicle < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  validates :license_plate, :name, :phone, :description, :type, :images, presence: true
+  validates :license_plate, uniqueness: true
+
+  enum type: {
+    deluxe: 'Deluxe',
+    ac: 'A/C',
+    air_suspension: 'Air Suspension',
+    sleeper: 'Sleeper Coach'
+  }
 end
